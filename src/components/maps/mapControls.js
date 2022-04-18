5cb797d728ae434890cd7703fa5a99a3216888e3
IvLaptev <ivlaptev13@ya.ru>
2022-04-18 06:21:05 +0300

    Added view mode to map

--- src/components/maps/mapControls.js
+++ src/components/maps/mapControls.js
@@ -51,41 +51,11 @@ export default {
     return { markers, areas, routes, lines: [areaLine, routeLine] }
   },
 
-  getData () {
-    const data = {
-      position: {
-        lat: markers[0].position.lat(),
-        lng: markers[0].position.lng()
-      },
-      areas: [],
-      routes: []
-    }
-
-    let error = null
-
-    if (!areas.length) {
-      error = 'Не задана ни одна область видимости'
-    }
-    if (areaLine && areaLine.getPath().getLength()) {
-      error = 'Не все области видимости завершены'
-    }
-    // if (!routeLine || !routeLine.getPath().getLength()) {
-    //   error = 'Не все маршруты завершены'
-    // }
-
-    areas.forEach((A) => {
-      const points = []
-      A.getPath().forEach((point) => points.push({ lat: point.lat(), lng: point.lng() }))
-      data.areas.push({ points })
-    })
-
-    return { data, error }
-  },
-
   map: {
     onClick (latLng) {
       if (mode === MODES.POSITION) {
         // Move marker to another place
+        if (!markers) markers = new Array(1)
         markers[0] = { position: latLng }
       } else if (mode === MODES.AREA) {
         areaLine.points.push(latLng)
@@ -156,60 +126,4 @@ export default {
   },
 
   route: {}
-}
-
-function removePoint (elem, latLng) {
-  const points = elem.getPath()
-  let pointToRemove = -1
-
-  points.forEach((point, i) => {
-    if (point.lat() === latLng.lat() && point.lng() === latLng.lng()) {
-      pointToRemove = i
-    }
-  })
-
-  if (pointToRemove !== -1) {
-    points.removeAt(pointToRemove)
-  }
-
-  elem.setPath(points)
-}
-
-function checkIfFinished(elem, latLng) {
-  if (mode === MODES.AREA) {
-    const points = elem.getPath()
-    const begining = points.getAt(0)
-    const ending = points.getAt(points.getLength() - 1)
-
-    if (
-      begining.lat() === latLng.lat() && begining.lng() === latLng.lng() ||
-      ending.lat() === latLng.lat() && ending.lng() === latLng.lng()
-    ) {
-      // eslint-disable-next-line
-      const area = new google.maps.Polygon({
-        path: points,
-        strokeColor: '#1976D2',
-        strokeWeight: 2,
-        fillColor: '#1976D2',
-        fillOpacity: 0.4,
-        editable: true
-      })
-
-      areaLine.setPath([])
-      area.addListener('rightclick', (e) => {
-        removePoint(area, e.latLng)
-      })
-      area.setMap(map)
-
-      areas.push(area)
-    }
-  }
-}
-
-function processCoordinate (latLng) {
-  if (mode === MODES.POSITION) {
-    // marker[0].setPosition(latLng)
-  } else if (mode === MODES.AREA) {
-    areaLine.setPath(areaLine.getPath().push(latLng))
-  }
 }
